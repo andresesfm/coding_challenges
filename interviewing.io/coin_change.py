0bdf8216f31a5516712474a5bb13b756d06b0113
Andres Aguilar <andres.aguilar@nfl.com>
2019-07-25 17:15:31 -0700

    incomplete second iteration

--- interviewing.io/coin_change.py
+++ interviewing.io/coin_change.py
@@ -10,7 +10,7 @@ def num_coins(cents: int) -> int:
         
     return coins
 
-print(num_coins(31))
+#print(num_coins(31))
 #first pass at dynamic programming answer general case
 def num_coins2(cents:int)-> int:
     vals = [25,10,1]
@@ -39,4 +39,44 @@ def num_coins2(cents:int)-> int:
         return m
     return helper(scounters,cents)
 
-print(num_coins2(31),'coins')
+#print(num_coins2(31),'coins')
+
+#second pass at dynamic programming answer general case: memoization
+def num_coins3(cents:int)-> int:
+    vals = [25,10,1]
+    scounters = [0,0,0]
+    memo = {}
+    currentVal = 0
+    MAX_VAL = 1000000
+    memo = {}
+    def value(counters) -> int:
+        v = 0
+        for c in range(len(vals)):
+            v += vals[c]* counters[c]
+        return v
+    def count(counters) -> int:
+        return sum([i for i in counters])
+    def helper(counters,rcents:int)-> int:
+        if rcents<0:
+            return -1
+        v = value(counters)
+        if rcents == 0:
+            return count(counters)
+        if v>rcents:
+            return MAX_VAL
+        if  v == rcents:
+            return count(counters)
+        m = MAX_VAL
+        all_neg = False
+        for i in range(len(counters)):
+            cm = helper([y+1 if x == i else y for x,y in enumerate(counters)],rcents-vals[i])
+            all_neg = all_neg and cm <0
+            if cm >=0 and cm < m:
+                m = cm
+        if all_neg:
+            m = -1
+        memo[rcents] = m
+        return m
+    return helper(scounters,cents)
+
+print(num_coins3(31),'coins')
