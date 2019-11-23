4496111d5f3fbb68611d03340a4b742285de2e46
Andres Aguilar <andres.aguilar@nfl.com>
2019-11-22 20:02:32 -0800

    DP word break works

--- leetcode.com/word-break.py
+++ leetcode.com/word-break.py
@@ -3,23 +3,21 @@ from typing import List
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        if n ==0:
-            return False
-        fits = True
-        for w in wordDict:
-            a = s
-            if w in s:
-                a = a.replace(w,'',1)
-                assert len(a) < len(s)
-                if len(a) > 0:
-                    fits = fits or self.wordBreak(a,wordDict)
-            else:
-                fits = fits or self.wordBreak(a,list(filter(lambda x: x!=w ,wordDict)))
-        return False
+        DP = [False for i in range(n+1)]
+
+        for i in range(1,n+1):
+            for w in wordDict:
+                wl = len(w)
+                if wl<i and w == s[i-wl:i]:
+                    DP[i] = DP[i] or DP[i-wl]
+                elif w == s[:i]:
+                    DP[i] = True
+        print(DP)
+        return DP[n]
 
 if __name__ == "__main__":
     s = Solution()
-    #print(s.wordBreak('leetcode',["leet", "code"]))
-    #print(s.wordBreak('applepenapple',["apple", "pen"]))
-    #print(s.wordBreak('catsandog',["cats", "dog", "sand", "and", "cat"]))
+    print(s.wordBreak('leetcode',["leet", "code"]))
+    print(s.wordBreak('applepenapple',["apple", "pen"]))
+    print(s.wordBreak('catsandog',["cats", "dog", "sand", "and", "cat"]))
     print(s.wordBreak("bb",["a","b","bbb","bbbb"]))
